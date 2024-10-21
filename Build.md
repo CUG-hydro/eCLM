5286a982dd9643c0c83a2cb51e56768d5207ded5
Dongdong Kong <kongdd@users.noreply.github.com>
2024-10-21 15:43:35 +0800

    works

---
+++ Build.md
@@ -0,0 +1,8 @@
+## 编译
+
+若编译出错，可尝试清理掉mct的编译文件。
+
+```bash
+cd src/externals/mct
+make clean
+```
--- julia/build.sh
+++ julia/build.sh
@@ -1,4 +1,5 @@
 gcc -c hello.c -o hello.o
 ar rcs libhello.a hello.o
 
+# 模型库不可行
 gcc -shared -o libhello.so -Wl,--whole-archive libhello.a -Wl,--no-whole-archive
